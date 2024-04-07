fb1f4008ab0c3589ba08d7a1d693c019c4549a56
PPeti1999 <pasztoripeti@hotmail.com>
2024-04-07 23:53:42 +0200

    jó az details is

--- ExerciseNote.WebAppUI/src/app/exercise-details-page/exercise-details-page.component.ts
+++ ExerciseNote.WebAppUI/src/app/exercise-details-page/exercise-details-page.component.ts
@@ -67,3 +67,81 @@ export class ExerciseDetailsPageComponent implements OnInit{
   }
 
 }
+/*
+htmL:
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="generator" content="Hugo 0.84.0">
+  
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" crossorigin="anonymous" />
+  
+    <!-- Custom styles for this template -->
+    <link href="news-details-page.component.css" rel="stylesheet">
+  </head>
+<body>
+
+  <div class="container mt-5">
+    <div class="row">
+        <div class="col-lg-8">
+            <!-- Post content-->
+            <article>
+                <header class="mb-4">
+                    <!-- Post title with larger font size -->
+                    <h1 class="fw-bolder mb-1" style="font-size: xx-large;">
+                        {{ exercise.title }}
+                        <!-- Big yellow star icon -->
+                        <!-- SVG csillag ikon -->
+                       
+                    </h1>
+                    <!-- Post meta content-->
+                    <div class="text-muted fst-italic mb-2">Posted on {{ exercise.created_At| date:'yyyy-MM-dd' }}</div>
+                    
+                    <!-- Post categories-->
+                    
+                    <a *ngIf="exercise?.type?.name" class="badge bg-secondary text-decoration-none link-light" >{{ exercise.type.name }}</a>
+                </header>
+            </article>
+        </div>
+    </div>
+</div>
+
+
+<main class="container-fluid">
+  <div class="row g-5">
+    <div class="col-md-8">
+      <h3 class="pb-4 mb-4 fst-italic border-bottom">
+        <div class="news-image">
+          <img src="{{ exercise.photo.photoData }}" alt="{{ exercise.title }}" class="img-fluid" />
+        </div>
+      </h3>
+
+      <article class="blog-post">
+    
+        <div class="news-description">
+          <p class="text-break">{{ exercise.body }}</p>
+        </div>
+      </article>
+
+      <nav class="blog-pagination" aria-label="Pagination">
+        <a class="btn btn-danger"  (click)="openDeleteModal()" tabindex="-1" aria-disabled="true">Delete</a>
+        <a class="btn btn-danger"  (click)="edit()" tabindex="-1" aria-disabled="true">Edit</a>
+      </nav>
+    </div>
+
+   
+  </div>
+</main>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
+*/
